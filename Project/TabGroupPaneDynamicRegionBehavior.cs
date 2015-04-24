724085272a26f78e4cce6a9d069b76b7f755e535
Oleg Merkulov <oleg.merkulov@baml.com>
2015-04-24 15:38:26 -0400

    WIP

---
+++ Project/TabGroupPaneDynamicRegionBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    /// <summary>
+    /// In this particular scenario, we want to have each newly created flaoting pane to act as it's own dynamic region.
+    /// To support this scenaio, we must first create a scoped region manager and region for each newly created TabGroupPane.
+    /// The TabGroupPane is created when 2 panes have been dragged into the same floating window.
+    /// </summary>
+    public class TabGroupPaneDynamicRegionBehavior : RegionBehavior, IHostAwareRegionBehavior
+    {
+        public const string BehaviorKey = "TabGroupPaneDynamicRegionBehavior";
+        public static readonly DependencyProperty IsScopedProperty =
+            DependencyProperty.RegisterAttached("IsScoped", typeof(bool), typeof(TabGroupPaneDynamicRegionBehavior), new PropertyMetadata(false));
+
+        TabGroupPane _hostControl;
+        XamDockManager _parentDockManager;
+
+        public DependencyObject HostControl
+        {
+            get { return _hostControl; }
+            set { _hostControl = value as TabGroupPane; }
+        }
+
+        public static bool GetIsScoped(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsScopedProperty);
+        }
+
+        public static void SetIsScoped(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsScopedProperty, value);
+        }
+
+        protected override void OnAttach()
+        {
+            //if we are dealing with a scoped region, then don;t hook to the xamDockManager events
+            var isScoped = GetIsScoped(HostControl);
+            if (!isScoped)
+            {
+                _parentDockManager = XamDockManager.GetDockManager(HostControl);
+                _parentDockManager.PaneDragEnded += DockManager_PaneDragEnded;
+            }
+        }
+
+        void DockManager_PaneDragEnded(object sender, PaneDragEndedEventArgs e)
+        {
+            var contentPane = e.Panes[0];
+            var tgp = contentPane.Parent as TabGroupPane; //make sure we are dealing with a TabgroupPane
+            if (tgp != null)
+            {
+                var regionName = RegionManager.GetRegionName(tgp); //if we already have a region, then we don't want to create another.
+                if (regionName == null)
+                {
+                    var scopedRegionManager = Region.RegionManager.CreateRegionManager(); //create a new region manager
+                    RegionManager.SetRegionManager(tgp, scopedRegionManager); //set the region manager to the newly created TabGroupPane
+                    RegionManager.SetRegionName(tgp, RegionNames.ToolSplitPanesDockableFloating); //now make the TabGrouPane a region    
+                    SetIsScoped(tgp, true); //mark this region as scoped
+                }
+            }
+        }
+    }
+}
---
+++ Project/XamRibbonRegionAdapter.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    [Export(typeof(XamRibbonRegionAdapter))]
+    public class XamRibbonRegionAdapter : RegionAdapterBase<XamRibbon>
+    {
+        private XamRibbon _xamRibbonRegionTarget;
+
+        [ImportingConstructor]
+        public XamRibbonRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory)
+            : base(regionBehaviorFactory)
+        { }
+
+        #region Overrides
+
+        protected override void Adapt(IRegion region, XamRibbon regionTarget)
+        {
+            _xamRibbonRegionTarget = regionTarget;
+
+            if (_xamRibbonRegionTarget.Tabs.Count > 0)
+            {
+                foreach (var tab in _xamRibbonRegionTarget.Tabs)
+                {
+                    region.Add(tab);
+                }
+
+                _xamRibbonRegionTarget.Tabs.Clear();
+            }
+
+            if (_xamRibbonRegionTarget.ContextualTabGroups.Count > 0)
+            {
+                foreach (var tabGroup in _xamRibbonRegionTarget.ContextualTabGroups)
+                {
+                    region.Add(tabGroup);
+                }
+
+                _xamRibbonRegionTarget.ContextualTabGroups.Clear();
+            }
+
+            foreach (var view in region.Views)
+            {
+                AddViewToRegion(view, _xamRibbonRegionTarget);
+            }
+
+            region.ActiveViews.CollectionChanged += (s, args) =>
+            {
+                switch (args.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        {
+                            foreach (var view in args.NewItems)
+                            {
+                                AddViewToRegion(view, _xamRibbonRegionTarget);
+                            }
+                            break;
+                        }
+                    case NotifyCollectionChangedAction.Remove:
+                        {
+                            foreach (var view in args.OldItems)
+                            {
+                                RemoveViewFromRegion(view, _xamRibbonRegionTarget);
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            // Do nothing.
+                            break;
+                        }
+                }
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new AllActiveRegion();
+        }
+
+        #endregion Overrides
+
+        #region Private
+
+        private static void AddViewToRegion(object view, XamRibbon xamRibbon)
+        {
+            var ribbonTabItem = view as RibbonTabItem;
+            if (ribbonTabItem != null)
+            {
+                xamRibbon.Tabs.Add(ribbonTabItem);
+            }
+            else
+            {
+                var contextualTabGroup = view as ContextualTabGroup;
+                if (contextualTabGroup != null)
+                {
+                    xamRibbon.ContextualTabGroups.Add(contextualTabGroup);
+                }
+            }
+        }
+
+        private static void RemoveViewFromRegion(object view, XamRibbon xamRibbon)
+        {
+            var ribbonTabItem = view as RibbonTabItem;
+            if (ribbonTabItem != null)
+            {
+                xamRibbon.Tabs.Remove(ribbonTabItem);
+            }
+            else
+            {
+                var contextualTabGroup = view as ContextualTabGroup;
+                if (contextualTabGroup != null)
+                {
+                    xamRibbon.ContextualTabGroups.Remove(contextualTabGroup);
+                }
+            }
+        }
+
+        #endregion Private
+    }
+}
